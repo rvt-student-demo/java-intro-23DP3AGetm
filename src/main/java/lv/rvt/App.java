7d7187fbd7df4d94d6e5ec13d347693be769bb51
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-09-18 07:01:24 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,23 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App 
+
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadiet 1.skaitlis:");
+        int number1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet 2.skaitli:");
+        int number2 = Integer.valueOf(scanner.nextLine());
+        int summa = number1 + number2;
+        int starp = number1 - number2;
+        int reiz = number1 * number2;
+        System.out.println(number1 + "+" + number2 + "=" + summa);
+        System.out.println(number1 + "-" + number2 + "=" + starp);
+        System.out.println(number1 + "*" + number2 + "=" + reiz);
+        double average = (number1 + number2)/2;
+        System.out.println("(" + number1 + "+"+number2 + ")/2=" + average);
     }
 
 }
