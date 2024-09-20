6722e27d41466a20140d56333b88905520b85291
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-09-20 05:51:51 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,18 +6,12 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ievadiet 1.skaitlis:");
-        int number1 = Integer.valueOf(scanner.nextLine());
-        System.out.println("Ievadiet 2.skaitli:");
-        int number2 = Integer.valueOf(scanner.nextLine());
-        int summa = number1 + number2;
-        int starp = number1 - number2;
-        int reiz = number1 * number2;
-        System.out.println(number1 + "+" + number2 + "=" + summa);
-        System.out.println(number1 + "-" + number2 + "=" + starp);
-        System.out.println(number1 + "*" + number2 + "=" + reiz);
-        double average = (number1 + number2)/2;
-        System.out.println("(" + number1 + "+"+number2 + ")/2=" + average);
+        System.out.println("Give speed:"); 
+        int speed = scanner.nextInt();
+        if (speed > 120){
+            System.out.println("Speeding ticket!");
+        }
     }
-
 }
+
+
