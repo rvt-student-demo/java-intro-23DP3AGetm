212f88020ffb81a44265df38fbd04636fe79b7f8
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-09-20 06:36:46 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,12 +6,30 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give speed:"); 
-        int speed = scanner.nextInt();
-        if (speed > 120){
-            System.out.println("Speeding ticket!");
+        System.out.println("Give points:");
+        int points = scanner.nextInt();
+        if(points < 0){
+            System.out.println("Impossible!");
+        }else if(points <= 49){
+            System.out.println("Failed!");
+        }else if(points <= 59){
+            System.out.println("1");
+        }else if(points <= 69){
+            System.out.println("2");
+        }else if(points <= 79){
+            System.out.println("3");
+        }else if(points <= 89){
+            System.out.println("4");
+        }else if(points <= 100){
+            System.out.println("5");
+        }else if (points > 100){
+            System.out.println("Incredible!");
+        }
+        
         }
     }
-}
+        
+
+
 
 
