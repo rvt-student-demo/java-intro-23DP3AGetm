84cff08cb00997e0f25305795f47579ddfb808b8
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-09-27 06:25:50 +0000

    Carry on?

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,23 +4,17 @@ public class App
 {
     public static void main(String[]args)
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Value of the gift?");
-        int Value = Integer.valueOf(scanner.nextInt());
-        if(Value>=1000000){
-            System.out.println("Tax:" + (142100 +(Value - 1000000) * 0.17));
-        }else if(Value>= 200000){
-            System.out.println("Tax:" + (22100+(Value - 200000) * 0.15));
-        }else if (Value>=55000){
-            System.out.println("Tax:" + (47000+(Value - 55000) * 0.12 ));
-        }else if (Value>=25000 ){
-            System.out.println("Tax:" + (1700+(Value - 25000) * 0.10 ));
-        }else if (Value>=5000){
-            System.out.println("Tax:" + (100+(Value - 5000) * 0.8 ));
-        }else if (Value>=3500){
-            System.out.println("No tax!");
+           Scanner scanner = new Scanner(System.in);
+           while(true){
+                System.out.println("Shall we carry on?");
+                String input = scanner.nextLine();
+                if (input.equals("no")){
+                    break;
+                }
+               
+            }
+            
         }
-}
 }
  
 
