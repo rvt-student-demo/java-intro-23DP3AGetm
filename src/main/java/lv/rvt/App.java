b5b2298ed7c20c387e136ce77fc38a4353409bb6
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-10-01 19:12:41 +0000

    Sum of a sequence

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,11 +5,13 @@ public class App
     public static void main(String[]args)
     {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Write number:");
+    System.out.println("Last number?");
     int number = scanner.nextInt();
-    for(int i = 0; i <=number; i++){
-        System.out.println(i);
+    int sum = 0;
+    for(int i = 1; i <= number;i++){
+        sum += i;
     }
+    System.out.println("The sum is " + sum);
     } 
     }
 
