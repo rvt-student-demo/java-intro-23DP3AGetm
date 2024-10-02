8f8e1e6dd35195121edb6fff4ac569fac6f768ae
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-10-02 07:11:58 +0000

    Repeating, breaking and remembering (5 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,19 +2,38 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
-    public static void main(String[]args)
-    {
+    public static void main(String[]args) {
+    
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Last number?");
-    int number = scanner.nextInt();
-    int sum = 0;
-    for(int i = 1; i <= number;i++){
-        sum += i;
-    }
-    System.out.println("The sum is " + sum);
-    } 
-    }
-
+    System.out.println("Give numbers:");
+    int summa = 0;
+    int result = 0;
+    double vid = 0;
+    int odd = 0;
+    int even = 0;
+    while(true){
+        int numbers = Integer.valueOf(scanner.nextLine());
+        if (numbers == -1){
+            System.out.println("Thx! Bye!");
+            System.out.println("Sum: " + summa);
+            System.out.println("Numbers: " + result);
+            System.out.println("Average: " + (double)summa/result);
+            System.out.println("Even: "+ even);
+            System.out.println("Odd: " + odd);
+            break;
+        }
+        summa += numbers;
+        result = result + 1;
+        if(numbers % 2 == 0){
+            even ++;
+        }
+        else{
+            odd ++;
+        }
+    }   
+    
+}
+}
 
  
 
