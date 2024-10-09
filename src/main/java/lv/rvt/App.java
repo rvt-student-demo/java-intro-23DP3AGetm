331cb37b270a991a1d922bf3407fe2dde7a52b6a
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-10-09 07:12:04 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,40 +2,21 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
-    public static void main(String[]args) {
-    
+public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Give numbers:");
-    int summa = 0;
-    int result = 0;
-    double vid = 0;
-    int odd = 0;
-    int even = 0;
-    while(true){
-        int numbers = Integer.valueOf(scanner.nextLine());
-        if (numbers == -1){
-            System.out.println("Thx! Bye!");
-            System.out.println("Sum: " + summa);
-            System.out.println("Numbers: " + result);
-            System.out.println("Average: " + (double)summa/result);
-            System.out.println("Even: "+ even);
-            System.out.println("Odd: " + odd);
-            break;
-        }
-        summa += numbers;
-        result = result + 1;
-        if(numbers % 2 == 0){
-            even ++;
-        }
-        else{
-            odd ++;
-        }
-    }   
-    
+    System.out.println("Write numbers:");
+    int beg = Integer.valueOf(scanner.nextLine());
+    int ends = Integer.valueOf(scanner.nextLine());
+    divisibleByThreeInRange(beg, ends);
+}
+public static void divisibleByThreeInRange(int beginning, int end){
+for(int number = beginning; number <= end;number++){
+    if (number % 3 == 0){
+        System.out.println(number);
+    }
 }
 }
-
- 
+}
 
 
 
