52dd6cb31c37a2e128b07bd21864dc907d8e2e07
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-10-16 07:35:20 +0000

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,46 +1,24 @@
 package lv.rvt;
 import java.util.Scanner;
-public class App {
+import java.util.ArrayList;
+public class App 
+{
 
-    public static void main(String[] args) {
-
-        
-      
-        
-        printStars(5);
-        printStars(3);
-        printStars(9);
-        System.out.println("\n");  
-        printSquare(4);
-        System.out.println("\n");
-        printRectangle(17, 3);
-        System.out.println("\n");
-        printTriangle(4);
-        System.out.println("\n");
-    }
-
-    public static void printStars(int number) {
-        for (int i = 0; i < number; i++) {
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-
-    public static void printSquare(int size) {
-        for (int i = 0; i < size; i++) {
-            printStars(size);
-        }
-    }
-
-    public static void printRectangle(int width, int height) {
-        for (int i = 0; i < height; i++) {
-            printStars(width);
-        }
-    }
-
-    public static void printTriangle(int size) {
-        for (int i = 1; i <= size; i++) {
-            printStars(i);
-        }
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> number = new ArrayList<>();
+        int sum = 0;
+        while(true){
+            System.out.println("Ievada skaitļi:");
+            int input = scanner.nextInt();
+            if(input ==0){
+                break;
+            }
+            number.add(input);
+            sum += input;
+            }
+        System.out.println("Summa:" + sum); 
     }
 }
+
