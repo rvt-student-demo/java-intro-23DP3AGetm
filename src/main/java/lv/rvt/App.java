c7039fea171cbae7963bcab18e1c3fcf93bf4443
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-10-23 06:42:34 +0000

    List size

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,19 +6,17 @@ public class App
 
     public static void main(String[] args) 
     {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> number = new ArrayList<>();
-        int sum = 0;
-        while(true){
-            System.out.println("Ievada skaitļi:");
-            int input = scanner.nextInt();
-            if(input ==0){
-                break;
-            }
-            number.add(input);
-            sum += input;
-            }
-        System.out.println("Summa:" + sum); 
+       Scanner scanner = new Scanner(System.in);
+       ArrayList<String> names = new ArrayList<>();
+       while(true){
+        System.out.println("Enter name: ");
+        String input = scanner.nextLine();
+        if(input.isEmpty()){
+            break;
+        }
+        names.add(input);
+       }
+       System.out.println("In total: " + names.size());
     }
 }
 
