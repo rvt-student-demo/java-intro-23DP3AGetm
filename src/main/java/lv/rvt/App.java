a823ccd98800592fb1f7e281583d9e3fe9e3f725
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-11-01 07:09:19 +0000

    Personal details

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,27 +1,70 @@
 package lv.rvt;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Random;
 public class App 
 {
 
     public static void main(String[] args) 
     {
+    //    Random random = new Random();
+    //    int[]numbers = new int[10];
+    //    for(int i = 0; i<numbers.length;i++){
+    //     numbers[i] = random.nextInt(10);
+    //    }
+    //    for(int num : numbers){
+    //     System.out.println(num);
+    //    }
+    //    int min = numbers[0];
+    //    int max = numbers[0];
+    //    int sum = 0;
+    //    for(int num : numbers){
+    //     if(num < min){
+    //         min = num;
+    //     }
+    //     if(num > max){
+    //         max = num; 
+    // }
+    //    }
+    //    double avarage = (double) sum/numbers.length;
+    //    for(int num : numbers){
+    //     System.out.println(num);
+    //    }
+    //    System.out.println("Max: " + max);
+    //    System.out.println("Min: " + min);
+    //    System.out.println("Avarage: " + avarage);
+
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Tank capacity: ");
-        int capacity = scanner.nextInt();
-        System.out.println("Gage reading: ");
-        int Gage = scanner.nextInt();
-        System.out.println("Miles per gallon: ");
-        int Miles = scanner.nextInt();
-        int distance = Gage * Miles;
-        if (distance>=200){
-            System.out.println("Get Gas!");
-        }else{
-            System.out.println("Safe to Proceed!");
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Integer> years = new ArrayList<>();
+        System.out.println("Enter name and birthday: ");
+        while (true) {
+            String input = scanner.nextLine();
+            if(input.isEmpty()){
+                break;
+            }
+            String[] parts = input.split(", ");
+            String name = parts[0];
+            int year = Integer.valueOf(parts[1]);
+            names.add(name);
+            years.add(year);
         }
+        String longname = "";
+        int TotalYears = 0;
+        for(String name : names){
+            if (name.length()>longname.length()){
+                longname = name;
+            }
+        }
+        for(int year : years){
+            TotalYears += year; 
+        }
+        double avarageYear = (double) TotalYears / years.size();
+        System.out.println("Longest name: " + longname);
+        System.out.println("Avarage of the birth years: " + avarageYear);
     }
 
-        
 }
 
 
