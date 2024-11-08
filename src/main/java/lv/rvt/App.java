6a84bf5e67095625830bfa3f09a5e741f4df4905
23DP3AGetm <166131665+23DP3AGetm@users.noreply.github.com>
2024-11-08 07:53:23 +0000

    Your first account

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,67 +4,24 @@ import java.util.ArrayList;
 import java.util.Random;
 public class App 
 {
-
     public static void main(String[] args) 
     {
-    //    Random random = new Random();
-    //    int[]numbers = new int[10];
-    //    for(int i = 0; i<numbers.length;i++){
-    //     numbers[i] = random.nextInt(10);
-    //    }
-    //    for(int num : numbers){
-    //     System.out.println(num);
-    //    }
-    //    int min = numbers[0];
-    //    int max = numbers[0];
-    //    int sum = 0;
-    //    for(int num : numbers){
-    //     if(num < min){
-    //         min = num;
-    //     }
-    //     if(num > max){
-    //         max = num; 
-    // }
-    //    }
-    //    double avarage = (double) sum/numbers.length;
-    //    for(int num : numbers){
-    //     System.out.println(num);
-    //    }
-    //    System.out.println("Max: " + max);
-    //    System.out.println("Min: " + min);
-    //    System.out.println("Avarage: " + avarage);
+        Account artosAccount = new Account("Arto's account", 100.00);
+        Account artosSwissAccount = new Account("Arto's account in Switzerland", 1000000.00);
 
+        System.out.println("Initial state");
+        System.out.println(artosAccount);
+        System.out.println(artosSwissAccount);
 
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<Integer> years = new ArrayList<>();
-        System.out.println("Enter name and birthday: ");
-        while (true) {
-            String input = scanner.nextLine();
-            if(input.isEmpty()){
-                break;
-            }
-            String[] parts = input.split(", ");
-            String name = parts[0];
-            int year = Integer.valueOf(parts[1]);
-            names.add(name);
-            years.add(year);
-        }
-        String longname = "";
-        int TotalYears = 0;
-        for(String name : names){
-            if (name.length()>longname.length()){
-                longname = name;
-            }
-        }
-        for(int year : years){
-            TotalYears += year; 
-        }
-        double avarageYear = (double) TotalYears / years.size();
-        System.out.println("Longest name: " + longname);
-        System.out.println("Avarage of the birth years: " + avarageYear);
+        artosAccount.withdraw(20);
+        System.out.println("The balance of Arto's account is now: " + artosAccount.balance());
+        artosSwissAccount.deposit(200);
+        System.out.println("The balance of Arto's other account is now: " + artosSwissAccount.balance());
+
+        System.out.println("End state");
+        System.out.println(artosAccount);
+        System.out.println(artosSwissAccount);
     }
-
 }
 
 
